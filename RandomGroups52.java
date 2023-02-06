4503fb0438c6fca97041b7a180ae9e27948e2ff1
alothmanmj <alothman.maj@gmail.com>
2023-02-06 14:42:38 +0300

    Build the reader

--- RandomGroups52.java
+++ RandomGroups52.java
@@ -1,5 +1,28 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class RandomGroups52{
     public static void main(String[] args) {
         System.out.println("Works fine");
+
+        System.out.println(readStudentList());
+    }
+
+    public static ArrayList<String> readStudentList(){
+        ArrayList<String> arr = new ArrayList<>();
+        try {
+            File f = new File("Student List.txt");
+            Scanner sc = new Scanner(f);
+            while(sc.hasNextLine()){
+                arr.add(sc.nextLine());
+            }
+
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+
+        return arr;
     }
 }
